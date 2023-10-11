2407962b370bf90c1f8a2033237028524710afe1
IgorMushk <igor.mushketik@gmail.com>
2023-10-11 17:17:01 +0300

    Operations -  add reducers
    Добавим редюсер для обработки каждого из возможных состояний запроса.

--- src/redux/taskSlice.js
+++ src/redux/taskSlice.js
@@ -1,9 +1,16 @@
 const tasksSlice = {
-    name: "tasks",
-    initialState: {
-      items: [],
-      isLoading: false,
-      error: null,
-    },
-    reducers: {},
-  };
+  name: 'tasks',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    // Выполнится в момент старта HTTP-запроса
+    fetchingInProgress(state) {},
+    // Выполнится если HTTP-запрос завершился успешно
+    fetchingSuccess() {},
+    // Выполнится если HTTP-запрос завершился с ошибкой
+    fetchingError() {},
+  },
+};
