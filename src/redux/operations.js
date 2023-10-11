552cc700ee945156e6884c02afc0351283f0389d
IgorMushk <igor.mushketik@gmail.com>
2023-10-11 23:04:50 +0300

    createAsyncThunk -  operations.js
    Осталось добавить обработку запроса завершившегося с ошибкой. Для этого необходимо дополнить код создания операции fetchTasks так, чтобы в случае ошибки запроса возвращался промис, который будет отклонен. Тогда на экшене ошибки запроса появится свойство payload.
    
    Колбэк функция, в которой выполняется запрос, называется payloadCreator и отвечает за составление значения свойства payload. Она будет вызвана с двумя аргументами: arg и thunkAPI.
    
    payloadCreator(arg, thunkAPI)
    
    arg - значение, которое было передано операции при вызове. Используется, например, для передачи идентификаторов объектов при удаления, текста заметки при создании и т. п.
    
    thunkAPI - объект, который передается в асинхронный генератор экшена в redux-thunk. Содержит свойства и методы доступа к стору, отправки экшенов, а также некоторые дополнительные.

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -22,7 +22,19 @@ axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 //   }
 // };
 
-export const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/tasks');
-  return response.data;
-});
+export const fetchTasks = createAsyncThunk(
+  'tasks/fetchAll',
+  // Используем символ подчеркивания как имя первого параметра,
+  // потому что в этой операции он нам не нужен
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/tasks');
+      // При успешном запросе возвращаем промис с данными
+      return response.data;
+    } catch (error) {
+      // При ошибке запроса возвращаем промис
+      // который будет отклонен с текстом ошибки
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
