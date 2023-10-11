c801332a5fc8aa3266d2c68d48afdab9a945efef
IgorMushk <igor.mushketik@gmail.com>
2023-10-12 01:05:02 +0300

    Reducing the reducer code - tasksSlice.js
    Код редюсеров, обрабатывающих pending и rejected экшены всех операций, идентичен. Вынесем логику этих редюсеров в функции, что поможет нам сократить дублирование кода.

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,44 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchTasks, addTask, deleteTask, toggleCompleted } from './operations';
 
-const tasksInitialState = {
-  items: [],
-  isLoading: false,
-  error: null,
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
 };
 
 const tasksSlice = createSlice({
-  name: 'tasks',
-  initialState: tasksInitialState,
-  // Добавляем обработку внешних экшенов
+  name: "tasks",
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   extraReducers: {
-    [fetchTasks.panding](state, action) {
-      state.isLoading = true;
-    },
+    [fetchTasks.pending]: handlePending,
     [fetchTasks.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchTasks.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [addTask.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchTasks.rejected]: handleRejected,
+    [addTask.pending]: handlePending,
     [addTask.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
     },
-    [addTask.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [deleteTask.pending](state) {
-      state.isLoading = true;
-    },
+    [addTask.rejected]: handleRejected,
+    [deleteTask.pending]: handlePending,
     [deleteTask.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -47,13 +41,8 @@ const tasksSlice = createSlice({
       );
       state.items.splice(index, 1);
     },
-    [deleteTask.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [toggleCompleted.pending](state) {
-      state.isLoading = true;
-    },
+    [deleteTask.rejected]: handleRejected,
+    [toggleCompleted.pending]: handlePending,
     [toggleCompleted.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -62,12 +51,79 @@ const tasksSlice = createSlice({
       );
       state.items.splice(index, 1, action.payload);
     },
-    [toggleCompleted.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-
+    [toggleCompleted.rejected]: handleRejected,
   },
 });
 
-export const tasksReduser = tasksSlice.reducer;
+export const tasksReducer = tasksSlice.reducer;
+
+// const tasksInitialState = {
+//   items: [],
+//   isLoading: false,
+//   error: null,
+// };
+
+// const tasksSlice = createSlice({
+//   name: 'tasks',
+//   initialState: tasksInitialState,
+//   // Добавляем обработку внешних экшенов
+//   extraReducers: {
+//     [fetchTasks.panding](state, action) {
+//       state.isLoading = true;
+//     },
+//     [fetchTasks.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items = action.payload;
+//     },
+//     [fetchTasks.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//     [addTask.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [addTask.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items.push(action.payload);
+//     },
+//     [addTask.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//     [deleteTask.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [deleteTask.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       const index = state.items.findIndex(
+//         task => task.id === action.payload.id
+//       );
+//       state.items.splice(index, 1);
+//     },
+//     [deleteTask.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//     [toggleCompleted.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [toggleCompleted.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       const index = state.items.findIndex(
+//         task => task.id === action.payload.id
+//       );
+//       state.items.splice(index, 1, action.payload);
+//     },
+//     [toggleCompleted.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+
+//   },
+// });
+
+// export const tasksReduser = tasksSlice.reducer;
