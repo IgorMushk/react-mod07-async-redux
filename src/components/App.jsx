6875c22976243294844297d384b5901ff625e912
IgorMushk <igor.mushketik@gmail.com>
2023-10-12 00:08:56 +0300

    Task Manager - ToDo -  App.js - read tasks
    Чтение задач
    Операция и редюсеры для чтения массива задач у нас уже есть. Дополним компонент App так, чтобы при его монтировании запускалась операция запроса за списком задач.
    
    После монтирования компонента App и завершения запроса, в интерфейсе отобразится список задач - компонент TaskList, который использует селекторы для получения массива задач из состояния Redux.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
+import { useDispatch } from "react-redux";
 import { AppBar } from "./AppBar/AppBar";
 import { Layout } from "./Layout/Layout";
 import { TaskForm } from "./TaskForm/TaskForm";
+import { useEffect } from "react";
 import { TaskList } from "./TaskList/TaskList";
+import { fetchTasks } from "redux/operations";
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+
   return (
     <Layout>
       <AppBar />
