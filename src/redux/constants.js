e57dcd033358693df55ae8b899f49f76eab8368d
IgorMushk <igor.mushketik@gmail.com>
2023-10-11 23:55:37 +0300

    del comments in dir Redux
    del actions.js
    del reducer.js
    
    old constants.js
    old filterSlise.js
    old selectors.js
    old stote.js
    
    === raplace  /redux react-mod06-redux-toolkit
    === on /redux _copy-react-mod07-async-redux-(Oparation-createAsynkThunk)
    
    new operatin s.js
    rapl tassksSlice.js
    
    https://codesandbox.io/s/goit-react-textbook-lesson-13-redux-toolkit-app-starter-code-0rmpkl?from-embed=&file=/src/redux/constants.js

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -4,11 +4,3 @@ export const statusFilters = {
     completed: "completed",
   };
   
-//   export const statusFilters = Object.freeze({
-//     all: "all",
-//     active: "active",
-//     completed: "completed",
-//   });
-
-// OBJECT.FREEZE()
-// Используем метод Object.freeze() для того, чтобы «заморозить» объект значений фильтра и предотвратить случайное его изменение по ссылке в местах импорта.
