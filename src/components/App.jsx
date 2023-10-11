57c43a3b6b24ab11668e481db757dfb71d172e6d
IgorMushk <igor.mushketik@gmail.com>
2023-10-12 00:13:06 +0300

    Task Manager - ToDo -  App.js  add Query indicator
    Индикатор запроса
    Добавим отображение индикатора запроса над списком задач.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppBar } from "./AppBar/AppBar";
 import { Layout } from "./Layout/Layout";
 import { TaskForm } from "./TaskForm/TaskForm";
 import { useEffect } from "react";
 import { TaskList } from "./TaskList/TaskList";
 import { fetchTasks } from "redux/operations";
+import { getError, getIsLoading } from "redux/selectors";
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -18,6 +21,7 @@ export const App = () => {
     <Layout>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </Layout>
   );
